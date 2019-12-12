31b3d92e0385e815bb2679eb5f6cf92914801368
Han Zhang <41025882+hanwavefront@users.noreply.github.com>
2019-12-12 13:16:02 -0800

    Add Jaeger-to-WF propagator

---
+++ src/Wavefront.OpenTracing.SDK.CSharp/Propagation/JaegerWavefrontPropagator.cs
@@ -0,0 +1,195 @@
+﻿using OpenTracing.Propagation;
+using System;
+using System.Collections.Generic;
+
+namespace Wavefront.OpenTracing.SDK.CSharp.Propagation
+{
+    /// <summary>
+    ///     Bridge for extracting/injecting Jaeger headers to/from a WavefrontSpanContext.
+    ///     
+    ///     Essentially allows for extracting Jaeger HTTP headers and creating a
+    ///     WavefrontSpanContext injecting Jaeger aware HTTP headers from a WavefrontSpanContext.
+    /// </summary>
+    public class JaegerWavefrontPropagator : IPropagator
+    {
+        private static readonly string BaggagePrefix = "baggage-";
+        private static readonly string TraceIdKey = "trace-id";
+        private static readonly string ParentIdKey = "parent-id";
+        private static readonly string SamplingDecisionKey = "sampling-decision";
+
+        private readonly string traceIdHeader;
+        private readonly string baggagePrefix;
+
+        private JaegerWavefrontPropagator(string traceIdHeader, string baggagePrefix)
+        {
+            this.traceIdHeader = traceIdHeader;
+            this.baggagePrefix = baggagePrefix;
+        }
+
+        /// <inheritdoc />
+        public void Inject<TCarrier>(WavefrontSpanContext spanContext, TCarrier carrier)
+        {
+            if (carrier is ITextMap textMap)
+            {
+                textMap.Set(traceIdHeader, ContextToTraceIdHeader(spanContext));
+                foreach (var entry in spanContext.GetBaggageItems())
+                {
+                    textMap.Set(baggagePrefix + entry.Key, entry.Value);
+                }
+                if (spanContext.IsSampled())
+                {
+                    textMap.Set(SamplingDecisionKey, spanContext.GetSamplingDecision().ToString());
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Invalid carrier " + carrier.GetType());
+            }
+        }
+
+        /// <inheritdoc />
+        public WavefrontSpanContext Extract<TCarrier>(TCarrier carrier)
+        {
+            if (carrier is ITextMap textMap)
+            {
+                Guid? traceId = null;
+                Guid? spanId = null;
+                string parentId = null;
+                var baggage = new Dictionary<string, string>();
+                bool? samplingDecision = null;
+
+                foreach (var entry in textMap)
+                {
+                    // normalize keys using invariant culture (i.e., independent of locale)
+                    string key = entry.Key.ToLowerInvariant();
+                    if (key.Equals(traceIdHeader.ToLowerInvariant()))
+                    {
+                        string[] traceData = ContextFromTraceIdHeader(entry.Value);
+                        if (traceData == null)
+                        {
+                            continue;
+                        }
+                        traceId = HexToGuid(traceData[0]);
+                        spanId = HexToGuid(traceData[1]);
+                        // setting parentId as current spanId
+                        parentId = spanId.HasValue ? spanId.Value.ToString() : null;
+                        samplingDecision = traceData[3].Equals("1");
+                    }
+                    else if (key.StartsWith(baggagePrefix.ToLowerInvariant()))
+                    {
+                        baggage.Add(StripPrefix(entry.Key), entry.Value);
+                    }
+                }
+
+                if (!traceId.HasValue || !spanId.HasValue)
+                {
+                    return null;
+                }
+                if (parentId.Trim().Length > 0 && !parentId.Trim().Equals("null"))
+                {
+                    baggage.Add(ParentIdKey, parentId);
+                }
+                return new WavefrontSpanContext(
+                    traceId.Value, spanId.Value, baggage, samplingDecision);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid carrier " + carrier.GetType());
+            }
+        }
+
+        /// <summary>
+        ///     Extracts traceId, spanId, parentId and samplingDecision from the 'uber-trace-id'
+        ///     HTTP header value that is in the format traceId:spanId:parentId:samplingDecision.
+        /// </summary>
+        private string[] ContextFromTraceIdHeader(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+            string[] ctx = value.Split(':');
+            if (ctx.Length != 4 || string.IsNullOrEmpty(ctx[0]))
+            {
+                return null;
+            }
+            return ctx;
+        }
+
+        /// <summary>
+        ///     Extracts traceId and spanId from a WavefrontSpanContext and constructs a Jaeger
+        ///     client compatible header of the form traceId:spanId:parentId:samplingDecision.
+        /// </summary>
+        private string ContextToTraceIdHeader(WavefrontSpanContext context)
+        {
+            string traceId = GuidToHex(context.GetTraceId());
+            string spanId = GuidToHex(context.GetSpanId());
+            bool samplingDecision = context.GetSamplingDecision() ?? false;
+            string decision = samplingDecision ? "1" : "0";
+            string parentId = context.GetBaggageItem(ParentIdKey);
+            return $"{traceId}:{spanId}:{parentId}:{decision}";
+        }
+
+        internal static string GuidToHex(Guid guid)
+        {
+            string hex = guid.ToString("N").TrimStart('0');
+            return hex.Length > 0 ? hex : "0";
+        }
+
+        internal static Guid? HexToGuid(string id)
+        {
+            if (id.Length > 32)
+            {
+                return null;
+            }
+            return Guid.Parse(new string('0', 32 - id.Length) + id);
+        }
+
+        private string StripPrefix(string key)
+        {
+            return key.Substring(baggagePrefix.Length);
+        }
+
+        /// <summary>
+        ///     A builder for <see cref="JaegerWavefrontPropagator"/> instances.
+        /// </summary>
+        public class Builder
+        {
+            private string traceIdHeader = TraceIdKey;
+            private string baggagePrefix = BaggagePrefix;
+
+            /// <summary>
+            ///     Sets a custom traceId header key. The default is "trace-id".
+            /// </summary>
+            /// <param name="traceIdHeader">The traceId header key.</param>
+            /// <returns><see cref="this"/></returns>
+            public Builder WithTraceIdHeader(string traceIdHeader)
+            {
+                this.traceIdHeader = traceIdHeader;
+                return this;
+            }
+
+            /// <summary>
+            ///     Sets a custom baggage prefix. The default is "baggage-".
+            /// </summary>
+            /// <param name="baggagePrefix">The baggage prefix.</param>
+            /// <returns><see cref="this"/></returns>
+            /// 
+            public Builder WithBaggagePrefix(string baggagePrefix)
+            {
+                this.baggagePrefix = baggagePrefix;
+                return this;
+            }
+
+            /// <summary>
+            ///     Builds and returns a <see cref="JaegerWavefrontPropagator"/> instance based on
+            ///     the given configuration.
+            /// </summary>
+            /// <returns>A <see cref="JaegerWavefrontPropagator"/>.</returns>
+            public JaegerWavefrontPropagator Build()
+            {
+                return new JaegerWavefrontPropagator(traceIdHeader, baggagePrefix);
+            }
+        }
+    }
+}
---
+++ test/Wavefront.OpenTracing.SDK.CSharp.Test/JaegerWavefrontPropagatorTest.cs
@@ -0,0 +1,78 @@
+﻿using OpenTracing.Propagation;
+using System;
+using System.Collections.Generic;
+using Wavefront.OpenTracing.SDK.CSharp.Propagation;
+using Xunit;
+
+namespace Wavefront.OpenTracing.SDK.CSharp.Test
+{
+    /// <summary>
+    ///     Unit tests for <see cref="JaegerWavefrontPropagator" />.
+    /// </summary>
+    public class JaegerWavefrontPropagatorTest
+    {
+        private static readonly string jaegerHeader = "uber-trace-id";
+
+        private readonly JaegerWavefrontPropagator wfJaegerPropagator =
+            new JaegerWavefrontPropagator.Builder()
+                .WithBaggagePrefix("uberctx-")
+                .WithTraceIdHeader(jaegerHeader)
+                .Build();
+
+        [Fact]
+        public void TestTraceIdExtract()
+        {
+            string val = "3871de7e09c53ae8:7499dd16d98ab60e:3771de7e09c55ae8:1";
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add(jaegerHeader, val);
+            var textMapExtractAdapter = new TextMapExtractAdapter(dictionary);
+            WavefrontSpanContext ctx = wfJaegerPropagator.Extract(textMapExtractAdapter);
+            Assert.NotNull(ctx);
+            Assert.Equal("00000000-0000-0000-3871-de7e09c53ae8", ctx.TraceId);
+            Assert.Equal("00000000-0000-0000-7499-dd16d98ab60e", ctx.SpanId);
+            Assert.Equal("00000000-0000-0000-7499-dd16d98ab60e", ctx.GetBaggageItem("parent-id"));
+            Assert.True(ctx.GetSamplingDecision() ?? false);
+        }
+
+        [Fact]
+        public void TestInvalidTraceIdExtract()
+        {
+            string val = ":7499dd16d98ab60e:3771de7e09c55ae8:1";
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add(jaegerHeader, val);
+            var textMapExtractAdapter = new TextMapExtractAdapter(dictionary);
+            WavefrontSpanContext ctx = wfJaegerPropagator.Extract(textMapExtractAdapter);
+            Assert.Null(ctx);
+        }
+
+        [Fact]
+        public void TestTraceIdInject()
+        {
+            var traceId = Guid.Parse("00000000-0000-0000-3871-de7e09c53ae8");
+            var spanId = Guid.Parse("00000000-0000-0000-7499-dd16d98ab60e");
+            var dictionary = new Dictionary<string, string>();
+            var textMapInjectAdapter = new TextMapInjectAdapter(dictionary);
+            wfJaegerPropagator.Inject(new WavefrontSpanContext(traceId, spanId, null, true),
+                textMapInjectAdapter);
+            Assert.True(dictionary.ContainsKey(jaegerHeader));
+            Assert.Equal("3871de7e09c53ae8:7499dd16d98ab60e::1", dictionary[jaegerHeader]);
+        }
+
+        [Fact]
+        public void TestHexToGuid()
+        {
+            string hex = "ef27b4b9f6e946f5ab2b47bbb24746c5";
+            Guid? guid = JaegerWavefrontPropagator.HexToGuid(hex);
+            Assert.Equal("ef27b4b9-f6e9-46f5-ab2b-47bbb24746c5",
+                guid.HasValue ? guid.Value.ToString() : null);
+        }
+
+        [Fact]
+        public void TestGuidToHex()
+        {
+            Guid guid = Guid.Parse("ef27b4b9-f6e9-46f5-ab2b-47bbb24746c5");
+            string hex = JaegerWavefrontPropagator.GuidToHex(guid);
+            Assert.Equal("ef27b4b9f6e946f5ab2b47bbb24746c5", hex);
+        }
+    }
+}
